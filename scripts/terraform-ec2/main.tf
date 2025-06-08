fca1338fa9d66ebc345f02be5b340c2257183252
Abhay Nayar <abhaynayar@gmail.com>
2025-06-08 18:29:57 +0530

    working on the node/terraform/ansible project..

--- scripts/terraform-ec2/main.tf
+++ scripts/terraform-ec2/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "ssh_access" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -12,7 +16,15 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # open to all — ok for testing
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -23,21 +35,21 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-
 resource "aws_instance" "my_ec2" {
   ami                         = "ami-02521d90e7410d9f0"  # Ubuntu 24.04 LTS
   instance_type               = "t2.micro"
   key_name                    = "ang-ap"
 
-  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+  vpc_security_group_ids      = [aws_security_group.web_access.id]
   associate_public_ip_address = true
 
   tags = {
-    Name = "MyTerraformEC2"
+    Name = "NodeAppServer"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
+  description = "Public IP of the EC2 instance"
 }
 
